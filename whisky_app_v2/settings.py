8518bd3cc7dfe9b956274fa32cedbe43866891ab
Kons <Konsatanasoff@gmail.com>
2022-12-20 05:16:46 +0200

    database init

--- whisky_app_v2/settings.py
+++ whisky_app_v2/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'whisky_app_v2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'ender3printer',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
