c55a505e3e09490ca76833cf79f26ff6093174b5
Kons <Konsatanasoff@gmail.com>
2022-12-20 06:08:47 +0200

    users_api setup

--- users_api/views.py
+++ users_api/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from users_api.models import UserModel
+from users_api.serializers import UserSerializer, LoginSerializer
+
+
+class UserListView(generics.ListAPIView):
+    # def get(self, request):
+    #     users = UserModel.objects.all()
+    #     serializer = UserSerializer(users, many=True)
+    #     return Response(serializer.data)
+    pass
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = UserModel.objects.all()
+
+
+# TODO
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = authenticate(**serializer.validated_data)
+        if user:
+            login(request, user)
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
